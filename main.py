16be0b636eaa50a9a8ec4a303c35fd123695843d
lian220 <lian.dy220@gmail.com>
2025-02-11 22:20:58 +0900

    second

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ st.title("ChatPDF")
 st.write("---")
 
 # 파일업로드
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a file", type=['pdf'])
 st.write("---")
 
 def pdf_to_document(uploaded_file):
