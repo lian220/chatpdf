e88b9d9fc4972ca5067b8b9e5c2793cbfe07dc79
lian220 <lian.dy220@gmail.com>
2025-02-14 22:23:59 +0900

    key 받기 추가

--- main.py
+++ main.py
@@ -14,10 +14,16 @@ from langchain.chains import RetrievalQA
 import streamlit as st
 import tempfile
 import os
+from streamlit_extras.buy_me_a_coffee import button
+button(username="lian220", floating=True, width=221)
 
 # 제목
 st.title("ChatPDF")
 st.write("---")
+
+#openai key입력 받기
+openai_key = st.text_input("open_ai_api_key", type="password")
+
 
 # 파일업로드
 uploaded_file = st.file_uploader("Choose a file", type=['pdf'])
@@ -48,7 +54,7 @@ if uploaded_file is not None:
     texts = text_splitter.split_documents(pages)
 
     #Embedding
-    embeddings_model = OpenAIEmbeddings()
+    embeddings_model = OpenAIEmbeddings(openai_api_key=openai_key)
 
     # load it into Chroma
     db = Chroma.from_documents(texts, embeddings_model, persist_directory="./chroma_db")
@@ -60,7 +66,7 @@ if uploaded_file is not None:
     if st.button("질문하기"):
         #Question
         with st.spinner("응답 하는 중...", show_time=True):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_key)
             qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
             result = qa_chain({"query": question})
             st.write(result["result"])
